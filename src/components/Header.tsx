fccedeeff1300d0aaad54a4a55384eb3825dded7
Andre Miras <andre.miras@gmail.com>
2021-05-30 11:50:08 +0200

    :lipstick: Adds Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="mb-4">
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a className="navbar-brand" href="/">Loan Calculator</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Home
+              <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="https://gitlab.com/andre.miras/loan-calculator">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default Header;
