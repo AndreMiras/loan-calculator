cc2035d50f556be82c7e86b3fc84c1c471dcd9e7
Andre Miras <andre.miras@gmail.com>
2021-06-06 00:48:54 +0200

    :recycle: Migrate header to react-bootstrap
    Fixes the burger menu at the same time.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = (): JSX.Element => (
-  <header className="mb-4">
-    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a className="navbar-brand" href="/">Loan Calculator</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">
-              Home
-              <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="https://gitlab.com/andre.miras/loan-calculator">About</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+  <Navbar bg="dark" variant="dark" expand="sm" className="mb-4">
+    <Container>
+      <Navbar.Brand href={process.env.PUBLIC_URL}>Loan Calculator</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav className="mr-auto">
+          <Nav.Link href={process.env.PUBLIC_URL}>Home</Nav.Link>
+          <Nav.Link href="https://gitlab.com/andre.miras/loan-calculator">About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 );
 
 export default Header;
