c432d015850974e07d8ae0b6c0a9ec73442366cc
Andre Miras <andre.miras@gmail.com>
2021-06-06 07:44:06 +0200

    :rotating_light: yarn format

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,47 @@
-import React, { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Row } from 'react-bootstrap';
-import Header from './components/Header';
-import LoanChart from './components/LoanChart';
-import LoanForm from './components/LoanForm';
-import LoanResult from './components/LoanResult';
-import { computeLoanParameters } from './utils/index';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Row } from "react-bootstrap";
+import Header from "./components/Header";
+import LoanChart from "./components/LoanChart";
+import LoanForm from "./components/LoanForm";
+import LoanResult from "./components/LoanResult";
+import { computeLoanParameters } from "./utils/index";
 
 const App = (): JSX.Element => {
   const [amount, setAmount] = useState(0);
   const [term, setTerm] = useState(0);
   const [interest, setInterest] = useState(0);
-  const { monthlyPayment, totalInterest, total } = computeLoanParameters(amount, term, interest);
+  const { monthlyPayment, totalInterest, total } = computeLoanParameters(
+    amount,
+    term,
+    interest
+  );
   return (
     <div className="App">
       <Header />
       <div className="container">
         <Row>
           <Col sm={12} md={4}>
-            <LoanForm onAmount={setAmount} onTerm={setTerm} onInterest={setInterest} />
+            <LoanForm
+              onAmount={setAmount}
+              onTerm={setTerm}
+              onInterest={setInterest}
+            />
           </Col>
           <Col>
-            <LoanResult monthlyPayment={monthlyPayment} amount={amount} totalInterest={totalInterest} total={total} />
+            <LoanResult
+              monthlyPayment={monthlyPayment}
+              amount={amount}
+              totalInterest={totalInterest}
+              total={total}
+            />
           </Col>
           <Col>
-            <LoanChart amount={amount} totalInterest={totalInterest} total={total} />
+            <LoanChart
+              amount={amount}
+              totalInterest={totalInterest}
+              total={total}
+            />
           </Col>
         </Row>
       </div>
--- src/components/LoanChart.tsx
+++ src/components/LoanChart.tsx
@@ -1,38 +1,50 @@
-import React, { FunctionComponent } from 'react';
-import { Doughnut } from 'react-chartjs-2';
-import { Card } from 'react-bootstrap';
+import React, { FunctionComponent } from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Card } from "react-bootstrap";
 
-const getLabels = (amount: number, totalInterest: number, total: number): [string, string]  => {
+const getLabels = (
+  amount: number,
+  totalInterest: number,
+  total: number
+): [string, string] => {
   const principalRatio = ((100 * amount) / total).toFixed(2);
   const interestRatio = ((100 * totalInterest) / total).toFixed(2);
   return [`Principal (${principalRatio}%)`, `Interest (${interestRatio}%)`];
 };
 
-const getChartParameters = (amount: number, totalInterest: number, total: number) => ({
+const getChartParameters = (
+  amount: number,
+  totalInterest: number,
+  total: number
+) => ({
   labels: getLabels(amount, totalInterest, total),
   datasets: [
     {
-      label: 'Loan distribution',
+      label: "Loan distribution",
       data: [amount, totalInterest],
-      backgroundColor: [
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 99, 132, 0.2)',
-      ],
+      backgroundColor: ["rgba(54, 162, 235, 0.2)", "rgba(255, 99, 132, 0.2)"],
     },
   ],
 });
 
 type LoanChartProps = {
-  amount: number,
-  totalInterest: number,
-  total: number,
+  amount: number;
+  totalInterest: number;
+  total: number;
 };
 
-const LoanChart: FunctionComponent<LoanChartProps> = ({ amount, totalInterest, total }) => (
+const LoanChart: FunctionComponent<LoanChartProps> = ({
+  amount,
+  totalInterest,
+  total,
+}) => (
   <Card className="mt-4">
     <Card.Header>Loan Distribution</Card.Header>
     <Card.Body>
-      <Doughnut data={getChartParameters(amount, totalInterest, total)} type="doughnut" />
+      <Doughnut
+        data={getChartParameters(amount, totalInterest, total)}
+        type="doughnut"
+      />
     </Card.Body>
   </Card>
 );
